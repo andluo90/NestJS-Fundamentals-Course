7cde839097f67ac7748b657aa2831974c09831b0
andluo <1455234749@qq.com>
2023-07-04 23:19:35 -0700

    注册服务细节

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+    constructor(
+        @InjectRepository(User) private readonly usersRepository:Repository<User>,
+        private readonly hashingService:HashingService,
+    ){}
+
+    async singUp(signUpDto:SignUpDto){
+        try {
+            const user = new User()
+            user.email = signUpDto.email
+            user.password = await this.hashingService.hash(signUpDto.password)
+            await this.usersRepository.save(user)            
+        } catch (error) {
+            const pgUniqueViolationErrorCode = '23505'
+            if(error.code === pgUniqueViolationErrorCode){
+                throw new ConflictException();
+            }
+            throw error
+        }
+
+    }
+
+}
