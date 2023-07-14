23e835f91b3739d6b99bb8e84605e4db5fe5c192
andluo <1455234749@qq.com>
2023-07-14 04:57:39 -0700

    响应拦截

---
+++ src/interceptors/TransformInterceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  resultCode:number,
+  resultObj:T
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(map(data => {
+        return {
+            resultCode:200,
+            resultObj:data
+        }
+    }));
+  }
+}
